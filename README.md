2acef7de3b6354360b72b4554ae4271ac3509202
Freewind <nowindlee@gmail.com>
2024-03-29 22:05:33 +0800

    typescript-html-rewrite-input-value-setter-demo

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-TypeScript HTML Demo
+TypeScript HTML Rewrite Input Value Setter Demo
 =================================
+
+如果直接调用 input.value = "value"，不会触发 MutationObserver。可以覆写set，手动 `setAttribute`来触发MutationObserver
 
 ```
 npm i
-npm run demo
+npm start
 ```
--- src/hello.ts
+++ src/hello.ts
@@ -1,1 +1,22 @@
-console.log('### Hello');
+const nativeInputSet = Object.getOwnPropertyDescriptor(HTMLInputElement.prototype, 'value')?.set;
+Object.defineProperty(HTMLInputElement.prototype, 'value', {
+    set: function (value) {
+        console.log("### set value", value);
+        this.setAttribute('value', value);
+        nativeInputSet?.call(this, value);
+    },
+});
+
+const input = document.querySelector('#name') as HTMLInputElement;
+
+input?.addEventListener('input', (event) => {
+    input.value = (event.target as HTMLInputElement).value;
+});
+
+new MutationObserver((mutations) => {
+    console.log("### MutationObserver", input.value);
+}).observe(document.body, {
+    childList: true,
+    subtree: true,
+    attributes: true,
+});
